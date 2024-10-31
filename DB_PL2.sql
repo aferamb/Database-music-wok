67efe2489a484663ca750807a6d94da2a6b31c28
alexpg2019 <05jandro@gmail.com>
2024-10-31 09:47:12 +0100

    Algunos errores de sintaxis en el archivo DB_PL2.sql

--- DB_PL2.sql
+++ DB_PL2.sql
@@ -2,10 +2,7 @@
 
 SET client_encoding = 'UTF8';
 
-CREATE DATABASE IF NOT EXISTS intercambio_discos
-    WITH OWNER = postgres
-    ENCODING = 'UTF8'
-    CONNECTION LIMIT = -1; -- -1 significa ilimitado
+
 
 /*
  * Buscar en la documentación oficial de PostgreSQL:
@@ -25,7 +22,7 @@ BEGIN;
 
 CREATE TABLE IF NOT EXISTS Grupo(
     Nombre TEXT NOT NULL,
-    Url_grupo TEXT -- NOT NULL
+    Url_grupo TEXT, -- NOT NULL
     CONSTRAINT pk_grupo PRIMARY KEY (Nombre)
 );
 
@@ -35,7 +32,7 @@ CREATE TABLE IF NOT EXISTS Disco(
     --Generos TEXT, -- MULTIEVALUADO no se ponen los generos
     Url_portada TEXT,
     Nombre_grupo TEXT NOT NULL,
-    CONSTRAINT pk_disco PRIMARY KEY (Titulo, Ano_publicacion)
+    CONSTRAINT pk_disco PRIMARY KEY (Titulo, Ano_publicacion),
     CONSTRAINT fk_disco_grupo FOREIGN KEY (Nombre_grupo) REFERENCES Grupo(Nombre) 
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -43,7 +40,7 @@ CREATE TABLE IF NOT EXISTS Disco(
 CREATE TABLE IF NOT EXISTS Generos(
     Titulo_disco TEXT,
     Ano_publicacion INT,
-    Genero TEXT,    
+    Genero TEXT,
     CONSTRAINT pk_genero PRIMARY KEY (Titulo_disco, Ano_publicacion, Genero) --revisar
     --CONSTRAINT fk_genero_disco FOREIGN KEY (Titulo_disco, Ano_publicacion) REFERENCES Disco(Titulo, Ano_publicacion) --revisar
     --ON DELETE RESTRICT ON UPDATE CASCADE
@@ -53,8 +50,8 @@ CREATE TABLE IF NOT EXISTS Canciones(
     Titulo_cancion TEXT NOT NULL,
     Titulo_disco TEXT NOT NULL,
     Ano_publicacion INT NOT NULL,
-    Duracion INT ,
-    CONSTRAINT pk_cancion PRIMARY KEY (Titulo_cancion, Titulo_disco, Ano_publicacion) 
+    Duracion INT,
+    CONSTRAINT pk_cancion PRIMARY KEY (Titulo_cancion, Titulo_disco, Ano_publicacion),
     CONSTRAINT fk_cancion_disco FOREIGN KEY (Titulo_disco, Ano_publicacion) REFERENCES Disco(Titulo, Ano_publicacion)
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -68,6 +65,14 @@ CREATE TABLE IF NOT EXISTS Ediciones(
     CONSTRAINT pk_edicion PRIMARY KEY (Titulo_disco, Ano_publicacion, Formato, Ano_edicion, Pais)
 );
 
+CREATE TABLE IF NOT EXISTS Usuario(
+    Nombre_user TEXT NOT NULL,
+    Nombre TEXT NOT NULL,
+    Email TEXT NOT NULL,
+    Contrasena TEXT NOT NULL,
+    CONSTRAINT pk_usuario PRIMARY KEY (Nombre_user)
+);
+
 CREATE TABLE IF NOT EXISTS Tiene(
     Nombre_user TEXT NOT NULL,
     Titulo_disco TEXT NOT NULL,
@@ -76,9 +81,9 @@ CREATE TABLE IF NOT EXISTS Tiene(
     Ano_edicion INT NOT NULL,
     Pais TEXT NOT NULL,
     Estado TEXT,
-    CONSTRAINT pk_tiene PRIMARY KEY (Nombre_user, Titulo_disco, Ano_publicacion, Formato, Ano_edicion, Pais)
+    CONSTRAINT pk_tiene PRIMARY KEY (Nombre_user, Titulo_disco, Ano_publicacion, Formato, Ano_edicion, Pais),
     CONSTRAINT fk_tiene_user FOREIGN KEY (Nombre_user) REFERENCES Usuario(Nombre_user)
-    ON DELETE RESTRICT ON UPDATE CASCADE
+    ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_tiene_disco FOREIGN KEY (Titulo_disco, Ano_publicacion, Formato, Ano_edicion, Pais) REFERENCES Ediciones(Titulo_disco, Ano_publicacion, Formato, Ano_edicion, Pais)
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -87,9 +92,9 @@ CREATE TABLE IF NOT EXISTS Desea(
     Titulo_disco TEXT NOT NULL,
     Ano_publicacion INT NOT NULL,
     Nombre_user TEXT NOT NULL,
-    CONSTRAINT pk_desea PRIMARY KEY (Titulo_disco, Ano_publicacion, Nombre_user)
+    CONSTRAINT pk_desea PRIMARY KEY (Titulo_disco, Ano_publicacion, Nombre_user),
     CONSTRAINT fk_desea_user FOREIGN KEY (Nombre_user) REFERENCES Usuario(Nombre_user)
-    ON DELETE RESTRICT ON UPDATE CASCADE
+    ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT fk_desea_disco FOREIGN KEY (Titulo_disco, Ano_publicacion) REFERENCES Disco(Titulo, Ano_publicacion)
     ON DELETE RESTRICT ON UPDATE CASCADE
 );
@@ -150,7 +155,7 @@ SET search_path='';
 \echo 'Cargando datos'
 -- COPY NOMBRE ARCHIVO FROM 'RUTA' DELIMITER 'DELIMITADOR' NULL 'NULL' CSV ENCODING 'UTF8';
 
-COPY canciones FROM 'Datos/canciones.csv' DELIMITER ';' NULL '' CSV ENCODING 'UTF8';
+COPY canciones_temp FROM 'Datos/canciones.csv' DELIMITER ';' NULL '' CSV ENCODING 'UTF8';
 COPY discos FROM 'Datos/discos.csv' DELIMITER ';' NULL '' CSV ENCODING 'UTF8';
 COPY ediciones FROM 'Datos/ediciones.csv' DELIMITER ';' NULL '' CSV ENCODING 'UTF8';
 COPY usuario_desea_disco FROM 'Datos/usuario_desea_disco.csv' DELIMITER ';' NULL '' CSV ENCODING 'UTF8';
