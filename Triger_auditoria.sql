43582d71ea57dcf67b1c79f52abfbde409692390
Alejandro <84287430+aferamb@users.noreply.github.com>
2025-01-04 11:52:16 +0100

    Estilizado

--- Triger_auditoria.sql
+++ Triger_auditoria.sql
@@ -17,23 +17,15 @@ CREATE OR REPLACE FUNCTION fn_auditoria() RETURNS TRIGGER AS $fn_auditoria$
   DECLARE
   --  no declaro nada porque no me hace falta...de hecho DECLARE podría haberlo omitido en éste caso
   BEGIN
-  -- Se determina que acción a activado el trigger e inserta un nuevo valor en la tabla dependiendo
-  -- del dicha acción
-  -- Junto con la acción se escribe fecha y hora en la que se ha producido la acción
-    IF TG_OP='INSERT' THEN
-      INSERT INTO auditoria VALUES (TG_TABLE_NAME, 'alta', SESSION_USER, current_timestamp);  -- Cuando hay una inserción
-    ELSIF TG_OP='UPDATE'	THEN
-      INSERT INTO auditoria VALUES (TG_TABLE_NAME, 'modificación', SESSION_USER, current_timestamp); -- Cuando hay una modificación
-    ELSEIF TG_OP='DELETE' THEN
-      INSERT INTO auditoria VALUES (TG_TABLE_NAME, 'borrado', SESSION_USER, current_timestamp); -- Cuando hay un borrado
-    END IF;	 
+  -- Insertar una fila en la tabla de auditoría con los valores de la tabla, operación, usuario y fecha actual
+    INSERT INTO auditoria VALUES (TG_TABLE_NAME, TG_OP, SESSION_USER, current_timestamp);
     RETURN NULL;
   END;
 $fn_auditoria$ LANGUAGE plpgsql;
 
 
 
--- Se crea el trigger que se dispara cuando hay una inserción, modificación o borrado en la tabla sala
+-- Se crea los trigger que se dispara cuando hay una inserción, modificación o borrado en la tabla sala
 
 CREATE TRIGGER tg_auditoria after INSERT or UPDATE or DELETE
   ON Grupo FOR EACH ROW
