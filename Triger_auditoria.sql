8680c7522acec64003e31195c67a54e989b49a81
Mariaalonsoarribas <maria.alonsoarribas@edu.uah.es>
2025-01-04 11:33:38 +0100

    Funcion disco

--- Triger_auditoria.sql
+++ Triger_auditoria.sql
@@ -1,3 +1,5 @@
+--Triger de auditoría
+
 CREATE TABLE IF NOT EXISTS auditoria (
     id INT AUTO_INCREMENT NOT NULL,
     tabla VARCHAR(100) NOT NULL,
@@ -36,3 +38,37 @@ $fn_auditoria$ LANGUAGE plpgsql;
 CREATE TRIGGER tg_auditoria after INSERT or UPDATE or DELETE
   ON nombre_tabla FOR EACH ROW
   EXECUTE PROCEDURE fn_auditoria(); 
+
+
+--Trigger disco
+
+-- Se crea la función que se ejecutará 
+
+CREATE OR REPLACE FUNCTION fn_eliminar_de_deseados() RETURNS TRIGGER AS $fn_eliminar_de_deseados$
+BEGIN
+    -- Comprobar si el disco insertado está en la lista de deseados del usuario
+    IF EXISTS (
+        SELECT 1
+        FROM lista_deseados
+        WHERE id_usuario = NEW.id_usuario
+          AND id_disco = NEW.id_disco
+    ) THEN
+        -- Eliminar el disco de la lista de deseados
+        DELETE FROM lista_deseados
+        WHERE id_usuario = NEW.id_usuario
+          AND id_disco = NEW.id_disco;
+    END IF;
+
+    -- Continuar con la inserción normalmente
+    RETURN NEW;
+END;
+$fn_eliminar_de_deseados$ LANGUAGE plpgsql;
+
+
+-- Se crea el trigger que se dispara cuando hay una inserción, modificación o borrado en la tabla sala
+
+CREATE TRIGGER tg_eliminar_de_deseados
+    AFTER INSERT
+    ON usuario_tiene_edicion
+    FOR EACH ROW
+    EXECUTE PROCEDURE fn_eliminar_de_deseados();
