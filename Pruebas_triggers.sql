1359b19e243c10ca97883233ca339b330c8d7c83
Alejandro <84287430+aferamb@users.noreply.github.com>
2025-01-04 12:33:33 +0100

    Los triggers funcionan correctamente, se han creado dos triggers, uno para la auditoria y otro para las pruebas de los triggers.

---
+++ Pruebas_triggers.sql
@@ -0,0 +1,13 @@
+
+INSERT INTO Grupo VALUES ('Atlanticos', 'www.hola.com');
+UPDATE Grupo SET web = 'www.adios.com' WHERE nombre = 'Atlanticos';
+DELETE FROM Grupo WHERE nombre = 'Atlanticos';
+
+INSERT INTO Usuario VALUES ('Alexito', 'Alex Ambros', 'holas@gmail.com', '1234');   
+INSERT INTO Desea VALUES ('The Cave', '2014', 'Alexito');
+SELECT * FROM Desea WHERE Nombre_user = 'Alexito' AND Titulo_disco = 'The Cave' AND Ano_publicacion = '2014';
+INSERT INTO Tiene VALUES ('Alexito', 'The Cave', '2014', 'Vinyl', '2014', 'US', 'M');
+
+
+SELECT * FROM auditoria;
+SELECT * FROM Desea WHERE Nombre_user = 'Alexito' AND Titulo_disco = 'The Cave' AND Ano_publicacion = '2014';
