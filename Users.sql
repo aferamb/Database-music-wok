9d680872449cd301ec93a997290e4ba05b7ccc79
Mariaalonsoarribas <maria.alonsoarribas@edu.uah.es>
2025-01-11 17:27:03 +0100

    Usuarios actualizado

--- Users.sql
+++ Users.sql
@@ -1,29 +1,32 @@
--- Crear el usuario administrador
-CREATE USER admin WITH PASSWORD 'password_admin'; 
-CREATE ROLE admin NOLOGIN;
-GRANT ALL PRIVILEGES ON DATABASE intercambio_discos TO admin;
-GRANT ALL PRIVILEGES ON SCHEMA public TO admin;
-GRANT CREATE ON SCHEMA public TO admin;
+--Crear los usuatios
+CREATE USER admin WITH PASSWORD 'admin';
+CREATE USER gestor WITH PASSWORD 'gestor';
+CREATE USER cliente WITH PASSWORD 'cliente';
+CREATE USER invitado WITH PASSWORD 'invitado';
 
--- Crear el usuario gestor y otorgar permisos para manipular los datos, pero no para modificar la estructura
-CREATE USER gestor WITH PASSWORD 'password_gestor';
+--Crear los roles
+CREATE ROLE admin NOLOGIN;
 CREATE ROLE gestor NOLOGIN;
-REVOKE ALL PRIVILEGES ON DATABASE intercambio_discos FROM gestor;
+CREATE ROLE cliente NOLOGIN;
+CREATE ROLE invitado NOLOGIN;
+
+--Asignar los permisos a los roles
+--Admin
+GRANT ALL PRIVILEGES ON DATABASE intercambio_discos TO admin;
+GRANT USAGE, CREATE ON SCHEMA public TO admin;
+GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO admin;
+
+--Gestor, otorgar permisos para manipular los datos, pero no para modificar la estructura
 GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO gestor;
+
 -- Asegurarse de que el usuario pueda interactuar con las tablas futuras también
 -- ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO gestor;
 
--- Crear el usuario cliente y otorgar permisos de consulta e inserción solo en las tablas específicas
-CREATE USER gumersindo WITH PASSWORD 'reconocer';
-CREATE ROLE gumersindo NOLOGIN;
-REVOKE ALL PRIVILEGES ON DATABASE intercambio_discos FROM gumersindo;
-GRANT SELECT, INSERT ON Tiene TO gumersindo;
-GRANT SELECT, INSERT ON Desea TO gumersindo;
+--Cliente
+GRANT SELECT, INSERT ON TABLE Tiene, Desea TO cliente;
 
--- Crear el usuario invitado y otorgar permisos de solo lectura (consulta) sobre las tablas específicas
-CREATE USER invitado WITH PASSWORD 'password_invitado';
-CREATE ROLE invitado NOLOGIN;
-REVOKE ALL PRIVILEGES ON DATABASE intercambio_discos FROM invitado;
-GRANT SELECT ON Grupo, Disco, Canciones TO invitado;
+--Cliente invitado
+GRANT SELECT ON TABLE Grupo, Disco, Canciones TO invitado;
+
 
 
